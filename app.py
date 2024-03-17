3a8138753f502303888280720d62bd15a5ca25c9
Thiago Villani <iamthiagovillani@gmail.com>
2024-03-17 01:04:21 -0300

    update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-
+"""apenas teste..."""
 def main():
-    st.markdown(":blue[Ola THIAGO VILLANI.]")
+    st.markdown(":blue[Ola ..............]")
 
 if __name__ == "__main__":
     main()
